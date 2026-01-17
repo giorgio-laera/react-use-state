d28295b5e526ec9495a29db2d471685c3d789bf9
giorgio laera <laeragiorg@gmail.com>
2026-01-17 11:14:25 +0100

    base

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [isOpen, setIsOpen] = useState(false);
+	function toggleAccordion() {
+		setIsOpen(isOpen => !isOpen);
+	}
 
-  return (
-    <>
-    
-    </>
-  )
+	return (
+		<>
+			<h1>Learn Web developer</h1>
+
+			<div className='Accordion'>
+				<div className='title'>
+					<button onClick={toggleAccordion}>HTML</button>
+				</div>
+				{isOpen && <div>HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</div>}
+			</div>
+			<div className='Accordion'>
+				<div className='title'>
+					<button onClick={toggleAccordion}>CSS</button>
+				</div>
+				{isOpen && <div>HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</div>}
+			</div>
+		</>
+	)
 }
 
 export default App
